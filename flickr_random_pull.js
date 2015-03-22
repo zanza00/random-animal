156ad744e7804b6edbae041fba0c40904b102829
Zanza00 <zanza00@gmail.com>
2015-03-22 18:30:12 +0100

    better text box

--- flickr_random_pull.js
+++ flickr_random_pull.js
@@ -6,8 +6,7 @@ function flickrPull() {
     var animalSearch = animalChoices[getRandomInt(0, animalChoices.length)];
     //cambio il titolo della pagina, mettendo il nome dell'animale con la prima lettera maiuscola
     document.title = animalSearch[0].toUpperCase() + animalSearch.slice(1) + ' Flikr Demo';
-    var urlForAPI = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6902943f0a9e5a3d4e84475e392ca8e7&format=json&nojsoncallback=1&sort=relevance&text=' + animalSearch;
-    $.getJSON(urlForAPI,
+    $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6902943f0a9e5a3d4e84475e392ca8e7&format=json&nojsoncallback=1&sort=relevance&text=' + animalSearch,
         function (data) {
             var randInt = getRandomInt(0, data.photos.perpage);
             $.each(data.photos.photo, function (i, item) {
@@ -22,10 +21,14 @@ function flickrPull() {
                         title: item.title,
                         id: 'animal-image'
                     }).appendTo("#image");
-                    $('<a>').attr({
-                        href: "http://www.flickr.com/photos/" + item.owner + "/" + item.id,
-                        id: 'link-text'
-                    }).appendTo('#text');
+                    //$('<a>').attr({
+                    //    href: "http://www.flickr.com/photos/" + item.owner + "/" + item.id,
+                    //    id: 'link-text'
+                    //}).appendTo('#text');
+                    $('#text').html(
+                        '<h2>' + item.title + '</h2>' +
+                        '<a href="http://www.flickr.com/photos/' + item.owner + '/' + item.id +'"> Link to Original image on Flikr</a>'
+                    );
                     $('a#link-text').text('Link to original photo on Flickr');
                     $('.loading').hide(400);
                     $('#background-image').css({
