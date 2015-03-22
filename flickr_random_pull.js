fcab8e03118793802901f4accbe8aa74af1f613b
Zanza00 <zanza00@gmail.com>
2015-03-22 19:12:32 +0100

    new title

--- flickr_random_pull.js
+++ flickr_random_pull.js
@@ -5,7 +5,7 @@ function flickrPull() {
     var animalChoices = ['owl', 'otter', 'alpaca', 'frog', 'cat'];
     var animalSearch = animalChoices[getRandomInt(0, animalChoices.length)];
     //cambio il titolo della pagina, mettendo il nome dell'animale con la prima lettera maiuscola
-    document.title = animalSearch[0].toUpperCase() + animalSearch.slice(1) + ' Flikr Demo';
+    document.title = animalSearch[0].toUpperCase() + animalSearch.slice(1) + ' is a Random Animal';
     $("#favicon").attr('href', 'icons/' + animalSearch + '.png');
     $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6902943f0a9e5a3d4e84475e392ca8e7&format=json&nojsoncallback=1&sort=relevance&text=' + animalSearch,
         function (data) {
